acd2135aec488a598dbd9fcd6281acc14569a35f
tayfurparmak <tayfur-parmak-1907@hotmail.com>
2023-12-01 02:07:53 +0300

    update

--- personApp/src/app/app.module.ts
+++ personApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PersonCreateComponent } from './components/person-create/person-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    PersonCreateComponent
   ],
   imports: [
     BrowserModule,
