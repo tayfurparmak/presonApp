b8bbeddf57d82de1a5ce51680d23633c57e09b1e
tayfurparmak <tayfur-parmak-1907@hotmail.com>
2023-12-01 01:53:24 +0300

    component created

---
+++ personApp/src/components/person-create/person-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-create',
+  templateUrl: './person-create.component.html',
+  styleUrl: './person-create.component.css'
+})
+export class PersonCreateComponent {
+
+}
